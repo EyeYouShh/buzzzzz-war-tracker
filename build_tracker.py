321bd61c07db4e2a238b0ee0089c65b304fb0ea4
EyeYouShh <50641026+EyeYouShh@users.noreply.github.com>
2026-07-07 14:00:08 -0400

    Auto-update war tracker 2026-07-07 18:00 UTC

--- build_tracker.py
+++ build_tracker.py
@@ -143,7 +143,7 @@ WAR_END_ISO = ""
 
 # ===== RAW WAR DATA (newest first) =====
 WAR_BLOCKS = [
-("717165433","7/7/26","REQ","30v30","""
+("817171029","7/8/26","КАЙФУН - ТАЙФУН","30v30","""
 #QL8CV0P0|gen|2|17|0|0|0|
 #QCUCLPPJV|Gr8Conqueror|3|17|0|0|0|
 #GQJUGLQRQ|stage6yo|4|17|0|0|0|
@@ -154,6 +154,39 @@ WAR_BLOCKS = [
 #G0VGRUCC|SwiftyKinja|10|15|0|0|0|
 #QP0CU0UC8|stage5yo|11|15|0|0|0|
 #GV80Y9L0Y|studkiller|12|15|0|0|0|
+#R00L0CY9C|MiniPekka|13|15|0|0|0|
+#RQCJR8JV|SurgeGold|14|15|0|0|0|
+#8J2V8998|DE1|16|15|0|0|0|
+#8G8QL98YV|StopImAsianBro|17|15|0|0|0|
+#GV088PV02|roham|18|15|0|0|0|
+#GRRYCUJP8|crimpo|19|14|0|0|0|
+#LGJ9RC9Y9|Halid #1|20|14|0|0|0|
+#PGLV2YQC|Kizaru|21|14|0|0|0|
+#GJLRURGC2|Cole|22|14|0|0|0|
+#Q9UJG0RJP|Sumairu|23|14|0|0|0|
+#GRGGPPQ8J|Pam from HR|24|14|0|0|0|
+#YLGQ20LP8|Brodie|25|14|0|0|0|
+#YVCJC0VCQ|Marrow|26|14|0|0|0|
+#8L9J0R2QJ|SWAGMUFFIN90|28|14|0|0|0|
+#GJ20RJ8RP|arius67'|29|14|0|0|0|
+#R0GCPRLPL|jj|33|13|0|0|0|
+#PRCQVCCV2|Brandon|34|13|0|0|0|
+#R0RV2P0U0|Pharah|35|13|0|0|0|
+#QLLPQV8VJ|uhlisuh|36|13|0|0|0|
+#QUJLUR02G|UNSTOPPABLE ADI|37|13|0|0|0|
+""", True, True),
+
+("717165433","7/7/26","REQ","30v30","""
+#QL8CV0P0|gen|2|17|0|0|0|
+#QCUCLPPJV|Gr8Conqueror|3|17|0|0|0|
+#GQJUGLQRQ|stage6yo|4|17|0|0|0|
+#209J8J0RL|Big Steppa|5|16|0|0|0|
+#82G8C2YJ9|Slime|6|16|0|0|0|
+#Y0UUGPRRU|Americanpatriot|8|16|0|0|0|
+#9V9QUCLJV|@INSTAGRAM|9|16|0|0|0|
+#G0VGRUCC|SwiftyKinja|10|15|0|0|0|
+#QP0CU0UC8|stage5yo|11|15|0|0|0|
+#GV80Y9L0Y|studkiller|12|15|1|3|3|14:3:3:18
 #R00L0CY9C|MiniPekka|13|15|0|0|0|
 #8J2V8998|DE1|14|15|0|0|0|
 #RQCJR8JV|SurgeGold|15|15|0|0|0|
@@ -202,12 +235,12 @@ WAR_BLOCKS = [
 #YLGQ20LP8|Brodie|25|14|1|1|1|10:1:1:18
 #YVCJC0VCQ|Marrow|26|14|1|1|1|11:1:1:18
 #GJ20RJ8RP|arius67'|28|14|1|2|2|32:2:2:17
-#8L9J0R2QJ|SWAGMUFFIN90|29|14|0|0|0|
+#8L9J0R2QJ|SWAGMUFFIN90|29|14|1|1|1|13:1:1:18
 #QGRPYC928|⚡️LSWreckless⚡️|30|14|1|2|2|14:2:2:18
 #QLYP90RPV|Ste|32|13|1|0|0|39:0:0:16
 #PRCQVCCV2|Brandon|34|13|1|0|0|19:0:0:17
 #R0RV2P0U0|Pharah|36|13|1|0|0|39:0:0:16
-""", True, True),
+""", False, True),
 
 ("517075682","7/5/26","U6","30v30","""
 #QL8CV0P0|gen|2|17|1|3|3|1:3:3:18
@@ -4617,6 +4650,7 @@ WAR_BLOCKS = [
 
 # ── Win/Loss/Draw results keyed by war ID ──
 RESULTS = {
+    "617080473": "L",
     "517075682": "W",
     "417055717": "L",
     "317054832": "L",
