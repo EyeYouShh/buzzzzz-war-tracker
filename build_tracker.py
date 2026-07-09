cbc822b213fae859bdcfcaddc9157e1ff7d194f9
EyeYouShh <50641026+EyeYouShh@users.noreply.github.com>
2026-07-09 14:34:36 -0400

    Auto-update war tracker 2026-07-09 18:34 UTC

--- build_tracker.py
+++ build_tracker.py
@@ -148,7 +148,7 @@ WAR_BLOCKS = [
 #QL8CV0P0|gen|2|17|0|0|0|
 #QCUCLPPJV|Gr8Conqueror|3|17|0|0|0|
 #GQJUGLQRQ|stage6yo|4|17|0|0|0|
-#209J8J0RL|Big Steppa|5|16|0|0|0|
+#209J8J0RL|Big Steppa|5|16|1|2|2|4:2:2:18
 #82G8C2YJ9|Slime|6|16|0|0|0|
 #Y0UUGPRRU|Americanpatriot|8|16|0|0|0|
 #9V9QUCLJV|@INSTAGRAM|9|16|0|0|0|
@@ -161,20 +161,20 @@ WAR_BLOCKS = [
 #8J2V8998|DE1|16|15|0|0|0|
 #8G8QL98YV|StopImAsianBro|17|15|0|0|0|
 #GRRYCUJP8|crimpo|18|15|0|0|0|
-#GV088PV02|roham|19|15|0|0|0|
+#GV088PV02|roham|19|15|1|2|2|18:2:2:15
 #LGJ9RC9Y9|Halid #1|20|14|0|0|0|
-#PGLV2YQC|Kizaru|21|14|0|0|0|
+#PGLV2YQC|Kizaru|21|14|1|3|3|20:3:3:15
 #GJLRURGC2|Cole|22|14|0|0|0|
 #GRGGPPQ8J|Pam from HR|23|14|0|0|0|
 #Q9UJG0RJP|Sumairu|24|14|0|0|0|
-#YLGQ20LP8|Brodie|25|14|0|0|0|
+#YLGQ20LP8|Brodie|25|14|1|3|3|25:3:3:14
 #YVCJC0VCQ|Marrow|26|14|0|0|0|
 #8L9J0R2QJ|SWAGMUFFIN90|27|14|0|0|0|
 #GJ20RJ8RP|arius67'|29|14|0|0|0|
 #PRCQVCCV2|Brandon|34|13|0|0|0|
-#R0RV2P0U0|Pharah|35|13|0|0|0|
-#QLLPQV8VJ|uhlisuh|36|13|0|0|0|
+#QLLPQV8VJ|uhlisuh|36|13|1|2|2|31:2:2:12
 #QUJLUR02G|UNSTOPPABLE ADI|37|13|0|0|0|
+#QUQ0RUG29|rinz|39|12|1|3|3|33:3:3:11
 """, True, True),
 
 ("817171029","7/8/26","КАЙФУН - ТАЙФУН","30v30","""
@@ -208,7 +208,7 @@ WAR_BLOCKS = [
 #R0RV2P0U0|Pharah|35|13|0|0|0|
 #QLLPQV8VJ|uhlisuh|36|13|1|3|3|37:3:3:12
 #QUJLUR02G|UNSTOPPABLE ADI|37|13|1|3|3|39:3:3:11
-""", True, True),
+""", False, True),
 
 ("717165433","7/7/26","REQ","30v30","""
 #QL8CV0P0|gen|2|17|1|3|3|1:3:3:18
@@ -4684,6 +4684,7 @@ WAR_BLOCKS = [
 
 # ── Win/Loss/Draw results keyed by war ID ──
 RESULTS = {
+    "817171029": "W",
     "717165433": "W",
     "617080473": "L",
     "517075682": "W",
